0a3e70ac28ecf40612e036c77fd126a5e255e210
Ras243 <nurrasuli4@gmail.com>
2022-02-22 20:32:40 +0800

    Day4_Java

---
+++ day4_Java_Stdin_and_Stdout_II.java
@@ -0,0 +1,19 @@
+package com.tutorial;
+import java.util.Scanner;
+
+public class day4_Java_Stdin_and_Stdout_II {
+    public static void main(String[] rascoding) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+        scan.close();
+
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
