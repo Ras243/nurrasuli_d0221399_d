245cfc261b699baabed537fde8cd0dc1addb0a36
Ras243 <nurrasuli4@gmail.com>
2022-04-10 20:56:39 +0800

    Day51 Java

---
+++ day51_Java_Exception_Handling_Try_catch.java
@@ -0,0 +1,22 @@
+package rascoding;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class day51_Java_Exception_Handling_Try_catch {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+try {
+    int x = scan.nextInt();
+    int y = scan.nextInt();
+    System.out.println(x/y);
+    
+}
+catch (InputMismatchException e) {
+    System.out.println(e.getClass().getName());
+}
+catch(ArithmeticException e){
+    System.out.println(e.getClass().getName()+": / by zero");
+}
+    }
+}
