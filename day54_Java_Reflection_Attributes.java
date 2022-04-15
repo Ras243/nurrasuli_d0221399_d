4b6c4523c99b96039442ab2cca0cf2eb5e8a7805
Ras243 <nurrasuli4@gmail.com>
2022-04-15 10:03:09 +0800

    Day54 Java

---
+++ day54_Java_Reflection_Attributes.java
@@ -0,0 +1,19 @@
+package rascoding;
+
+public class day54_Java_Reflection_Attributes {
+
+    public static void main(String[] args){
+        Class student = Student.class;
+        Method[] methods = student.getDeclaredMethods();
+
+        ArrayList<String> methodList = new ArrayList<>();
+        for(Method method : methods){
+            methodList.add(method.getName());
+        }
+        Collections.sort(methodList);
+        for(String name: methodList){
+            System.out.println(name);
+        }
+    }
+
+}
